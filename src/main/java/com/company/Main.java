f3c5b7dc89f75a271268a2f091eba9305f2ee2e9
Antoni Guss <39112558+AnthonyGu55@users.noreply.github.com>
2020-05-21 16:15:19 +0200

    Rewriten a lot of the code using JavaFX insead of Swing Added things like AlertBoxes and ConfirmBoxes Made it that values that aren't Integers aren't saved

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,122 +1,150 @@
 package com.company;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Main extends JFrame{
-    public String[] heroes = new String[]{"Bangalore", "Bloodhound", "Caustic", "Crypto", "Gibraltar", "Lifeline", "Loba", "Mirage",
-    "Octane", "Pathfinder", "Revenant", "Watson", "Wraith"};
-    public Main() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        final ArrayList <Game_Entry> game_entries = new ArrayList <>();
-        final JFrame frame = this;
-        final CSVSaver saver = new CSVSaver();
 
-        GUIPanel panel = new GUIPanel();
+public class Main extends Application {
 
-        JButton button = new JButton("Save");
+    Stage window;
+    CSVSaver saver;
+    ArrayList<Game_Entry> game_entries;
 
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent actionEvent) {
-                String hero_name = panel.getHero_name_box().getName();
-                int damage = Integer.parseInt(panel.getDamage_field().getText());
-                int kills = Integer.parseInt(panel.getKills_field().getText());
-                int position = Integer.parseInt(panel.getPosition_field().getText());
+    public String[] heroes = new String[]{"Bangalore", "Bloodhound", "Caustic", "Crypto", "Gibraltar",
+            "Lifeline", "Loba", "Mirage", "Octane", "Pathfinder", "Revenant", "Watson", "Wraith"};
 
-                game_entries.add(new Game_Entry(hero_name, damage, kills, position));
-                for(Game_Entry x:game_entries){
-                    System.out.println(x);
-                }
+    @Override
+    public void start (Stage stage){
+        game_entries = new ArrayList <>();
+        saver = new CSVSaver();
+        window = new Stage();
 
-            }
-        });
-        addWindowListener(new WindowAdapter()
-        {
-            @Override
-            public void windowClosing(WindowEvent e)
-            {
+
+        DataInputLayout dataInputLayout = new DataInputLayout();
+        //Buttons
+        Button button = new Button("Save");
+        button.setOnAction(e ->{
+
+                String hero_name = dataInputLayout.getHero_name_box().getValue();
                 try {
-                    saver.WriteArrayToFile(game_entries, "C:\\Users\\Antoni\\Programming\\IdeaProjects\\ApexStats\\game_entries.csv");
+                    int damage = Integer.parseInt(dataInputLayout.getDamage_field().getText());
+                    int kills = Integer.parseInt(dataInputLayout.getKills_field().getText());
+                    int position = Integer.parseInt(dataInputLayout.getPosition_field().getText());
+
+                    boolean anwser = ConfirmBox.display("New Entry", "Are you sure you want to add this entry?\n\tHero: " + hero_name + "\n\tDamage: " + damage +"\n\tKills: " + kills + "\n\tPosition: " + position);
+                    if(anwser) {
+                        game_entries.add(new Game_Entry(hero_name, damage, kills, position));
+
+                        dataInputLayout.getDamage_field().setText("");
+                        dataInputLayout.getKills_field().setText("");
+                        dataInputLayout.getPosition_field().setText("");
+                    }
+                    e.consume();
+                }catch(NumberFormatException exception){
+                    AlertBox.display("Wrong input", "Error: You entered a wrong value, which wasn't a number");
+                     e.consume();
                 }
-                catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-                System.out.println("Closed");
-                e.getWindow().dispose();
+
+        });
+        StackPane pane = new StackPane();
+        pane.getChildren().add(button);
+
+
+
+        //Layout
+        BorderPane border_layout = new BorderPane();
+        border_layout.setBottom(pane);
+        border_layout.setCenter(dataInputLayout);
+
+
+        window.setOnCloseRequest(e ->{
+            e.consume();
+            try {
+                closeProgram();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
             }
         });
 
-        this.add(panel, BorderLayout.CENTER);
-        frame.add(button, BorderLayout.SOUTH);
-
-        this.setVisible(true);
-        this.pack();
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
-
-public class GUIPanel extends JPanel{
-    private final JComboBox hero_name_box;
-    private final JTextField damage_field;
-    private final JTextField kills_field;
-    private final JTextField position_field;
-
-    public JComboBox getHero_name_box() {
-        return hero_name_box;
-    }
-
-    public JTextField getDamage_field() {
-        return damage_field;
-    }
-
-    public JTextField getKills_field() {
-        return kills_field;
-    }
-
-    public JTextField getPosition_field() {
-        return position_field;
-    }
-
-    public GUIPanel() {
-
-        JLabel hero_name_label = new JLabel("Hero Name");
-        hero_name_box = new JComboBox(heroes);
-
-        add(hero_name_label);
-        add(hero_name_box);
-
-        JLabel damage_label= new JLabel("Damage");
-        damage_field = new JTextField(5);
-        add(damage_label);
-        add(damage_field);
-
-        JLabel kills_label = new JLabel("Kills");
-        kills_field = new JTextField(2);
-        add(kills_label);
-        add(kills_field);
-
-        JLabel position_label = new JLabel("Position");
-        position_field = new JTextField(2);
-        add(position_label);
-        add(position_field);
-
+        Scene scene = new Scene(border_layout,900, 100);
+        window.setScene(scene);
+        window.show();
 
 
     }
 
-}
-    public static void main(String[] args){
-        new Main();
+    private void closeProgram () throws IOException {
+        saver.WriteArrayToFile(game_entries, "C:\\Users\\Antoni\\Programming\\IdeaProjects\\ApexStats\\game_entries.csv");
+        window.close();
+    }
 
+    public class DataInputLayout extends HBox {
+        private final ChoiceBox<String> hero_name_box ;
+        private final TextField damage_field;
+        private final TextField kills_field;
+        private final TextField position_field;
+
+        public ChoiceBox <String> getHero_name_box () {
+            return hero_name_box;
+        }
+
+        public TextField getDamage_field () {
+            return damage_field;
+        }
+
+        public TextField getKills_field () {
+            return kills_field;
+        }
+
+        public TextField getPosition_field () {
+            return position_field;
+        }
+
+        public DataInputLayout (){
+
+            Label hero_name_label = new Label("Hero");
+            hero_name_box = new ChoiceBox <>();
+            addArray(hero_name_box, heroes);
+            hero_name_box.setValue("Mirage");
+            this.getChildren().addAll(hero_name_label,hero_name_box);
+
+            Label damage_label = new Label("Damage");
+            damage_field = new TextField();
+            this.getChildren().addAll(damage_label,damage_field);
+
+            Label kills_label = new Label("Kills");
+            kills_field = new TextField();
+            this.getChildren().addAll(kills_label, kills_field);
+
+            Label position_label = new Label("Position");
+            position_field = new TextField();
+            this.getChildren().addAll(position_label, position_field);
+
+
+        }
+
+        public void addArray(ChoiceBox<String> hero_name_box, String[] heroes){
+            for(String s:heroes){
+                hero_name_box.getItems().add(s);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
